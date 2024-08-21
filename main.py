6bbd9fc6447c55eed6ac610a6d3757a6f4974145
thgcMtdh <kyouho315@gmail.com>
2024-08-21 23:48:41 +0900

    upd: [large] change to bleak. Properly working on my Win10

--- main.py
+++ main.py
@@ -7,15 +7,15 @@ Quick start:
         $ python3 -m venv venv
         $ source venv/bin/activate (Linux)
         $ venv/Scripts/activate    (Windows)
-        $ pip install bluepy
+        $ pip install bleak
     * Specify the absolute path where data is saved in `LOG_DIRECTORY` variable
       The folder structure should be:
         LOG_DIRECTORY
-          0123456789ab    # folder is automatically created. the name is each MAC address
+          0123456789AB    # folder is automatically created. the name is each MAC address
             20240801.csv  # files are separated by day
             20240802.csv
             ...
-          123456789abc
+          123456789ABC
             20240801.csv
             20240802.csv
             ...
@@ -24,124 +24,116 @@ Quick start:
         $ sudo venv/bin/python main.py (if permission is denied)
 """
 
+import os
 import datetime
-from bluepy.btle import Scanner
+import asyncio
+from bleak import BleakScanner
 
-# absolute path of data directory
-LOG_DIRECTORY = "/home/hibiki/Documents/temperatureLog"
+# [USER SETTING] absolute path of data directory
+LOG_DIRECTORY = "C:/Users/user/Documents/switchBotLog"
 
+# dictionary to store the previous value of each device
+# key: device MAC, value: tuple
+prev_val = {}
 
-def get_broadcast_message(device) -> tuple[str, str]:
-    manufacturer_data = ""
-    service_data = ""
-    for (adtype, desc, value) in device.getScanData():
-        if adtype == 0xff:
-            manufacturer_data = value
-        if adtype == 0x16:
-            service_data = value
-    return (manufacturer_data, service_data)
+# dictionary to store the latest data arraival time of each device
+# key: device MAC, value: datetime.datetime
+prev_time = {}
+
 
 def decode_plug_power(mf_data: bytes) -> float:
-    seqNum = mf_data[8]
-    state  = mf_data[9]
-    power  = ((mf_data[12] & 0x7f) << 8) + mf_data[13]
+    seqNum = mf_data[6]
+    state  = mf_data[7]
+    power  = ((mf_data[10] & 0x7f) << 8) + mf_data[11]
     power /= 10.0  # [W]
     return power
 
 def decode_meter_temp_and_hum(mf_data: bytes) -> tuple[float, float]:
-    temp_float = mf_data[10] & 0x0f
-    temp_int   = mf_data[11] & 0x7f
-    temp_sign  = 1 if (mf_data[11] & 0x80) == 0x80 else -1
+    temp_float = mf_data[8] & 0x0f
+    temp_int   = mf_data[9] & 0x7f
+    temp_sign  = 1 if (mf_data[9] & 0x80) == 0x80 else -1
     temp       = temp_sign * temp_int + 0.1 * temp_float
-    hum        = mf_data[12] & 0x7f
+    hum        = mf_data[10] & 0x7f
     return (temp, hum)
 
-def update_log_file(dir: str, time: datetime.datetime, value):
-    path = dir + "/" + time.strftime("%Y%m%d") + ".csv"
-    with open(path, "a", encoding="utf-8") as f:
-        row = time.strftime("%Y/%m/%d %H:%M:%S")
-        row += ","
-        row += ",".join([str(i) for i in value])  # tuple to str. eg) [12, 14] -> "12,14"
-        f.write(row + "\n")
+def is_value_different(devaddr, value) -> bool:
+    if prev_val.get(devaddr) == None:
+        return True
+    if prev_val.get(devaddr) != value:
+        return True
+    return False
 
-def main():
+def is_day_different(devaddr, now) -> bool:
+    if prev_time.get(devaddr) == None:
+        return True
+    if prev_time.get(devaddr).day != now.day:
+        return True
+    return False
 
-    # previous scan time
-    prev_time = None
+def update_log_file(address: str, time: datetime.datetime, value):
+    # write log only when different value has arrived or the first time after the day has changed
+    if is_value_different(address, value) or is_day_different(address, time):
+        dir_path = os.path.join(LOG_DIRECTORY, address.replace(":",""))
+        file_name = time.strftime("%Y%m%d") + ".csv"
+        os.makedirs(dir_path, exist_ok=True)  # create directory if not exist
+        with open(os.path.join(dir_path, file_name), "a", encoding="utf-8") as f:
+            row = time.strftime("%H:%M:%S.%f")[:-3]  # milli seconds
+            row += ","
+            row += ",".join([str(i) for i in value])  # tuple to str. eg) [12, 14] -> "12,14"
+            f.write(row + "\n")
+            print(address, row)
 
-    # dictionary to store the previous value of each device
-    # key: device MAC, value: tuple
-    prev_val = {}
 
-    def is_value_different(devaddr, value) -> bool:
-        if prev_val.get(devaddr) == None:
-            return True
-        if prev_val.get(devaddr) != value:
-            return True
-        return False
+async def main():
 
-    def is_day_different(now) -> bool:
-        if prev_time == None:
-            return True
-        if prev_time.day != now.day:
-            return True
-        return False
+    stop_event = asyncio.Event()
 
-    scanner = Scanner()
-    try:
-        while True:  # infinite loop to continuously scan BLE. press Ctrl+C to stop
-            now = datetime.datetime.now()
-            devices = scanner.scan(1)  # scan BLE advertising packets for 1 sec
+    def callback(device, advertising_data):
 
-            for dev in devices:
+        # data received time
+        now = datetime.datetime.now()
 
-                # get broadcast message from device
-                (manufacturer_data, service_data) = get_broadcast_message(dev)
+        # manufacturer_data is dictionary and the keys are Bluetooth SIG assigned Company Identifiers
+        # the company identifier of SwitchBot is 2409 (Woan Technology (Shenzhen) Co., Ltd.)
+        if 2409 in advertising_data.manufacturer_data:
 
-                # skip if the message wasn't retrieved properly
-                if manufacturer_data == "" or service_data == "":
-                    continue
-
-                # convert message(str) to bytes
-                manufacturer_data_bytes = bytes.fromhex(manufacturer_data)
-                service_data_bytes = bytes.fromhex(service_data)
+            # type of the device is contained in service_data[0]. so service data should exist
+            if advertising_data.service_data:
+                service_data_bytes = advertising_data.service_data['0000fd3d-0000-1000-8000-00805f9b34fb']
+                manufacturer_data_bytes = advertising_data.manufacturer_data[2409]
 
                 # Meter device
-                if service_data_bytes[2] == 0x54 or service_data_bytes[2] == 0x77:
+                if service_data_bytes[0] == 0x54 or service_data_bytes[0] == 0x77:
                     # get temperature and humidity from data bytes
                     (temp, hum) = decode_meter_temp_and_hum(manufacturer_data_bytes)
 
                     # write log only when different value has arrived or day has changed
-                    if is_value_different(dev.addr, (temp, hum)) or is_day_different(now):
+                    if is_value_different(device.address, (temp, hum)) or is_day_different(device.address, now):
                         update_log_file(
-                            LOG_DIRECTORY + "/" + dev.addr.replace(":",""),
+                            device.address,
                             now,
                             (temp, hum)
                         )
-                        prev_val[dev.addr] = (temp, hum)
+                        prev_val[device.address] = (temp, hum)
+                        prev_time[device.address] = now
                 
                 # Plug device
-                if service_data_bytes[2] == 0x6a:
+                if service_data_bytes[0] == 0x6a:
                     # get power from data bytes
                     power = decode_plug_power(manufacturer_data_bytes)
-                    # if power < 0.5:  # my aircon repeats 0.3 and 0.0 on standby so ignore small value
-                    #     power = 0.0
 
                     # write log only when different value has arrived or day has changed
-                    if is_value_different(dev.addr, (power, )) or is_day_different(now):
+                    if is_value_different(device.address, (power, )) or is_day_different(device.address, now):
                         update_log_file(
-                            LOG_DIRECTORY + "/" + dev.addr.replace(":",""),
+                            device.address,
                             now,
                             (power, )
                         )
-                        prev_val[dev.addr] = (power, )
-        
-            prev_time = now
+                        prev_val[device.address] = (power, )
+                        prev_time[device.address] = now
+
+    async with BleakScanner(callback) as scanner:
+        await stop_event.wait()
 
 
-    except KeyboardInterrupt:
-        print("finish")
-
-
-if __name__ == "__main__":
-    main()
+asyncio.run(main())
